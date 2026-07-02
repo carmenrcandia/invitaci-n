8f4ff0866ff4b297afb1d8f13777058a31e1ad74
Carmen Candia <carmenreynacandiasunagua5@gmail.com>
2026-07-02 13:47:48 -0400

    Add files via upload

---
+++ estilo2.js
@@ -0,0 +1,9 @@
+<script>
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+        document.getElementById("asistente").classList.add("mostrar");
+    }, 5000); // 5 segundos
+
+});
+</script>
